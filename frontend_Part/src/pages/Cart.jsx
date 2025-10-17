d468ab527ba4b4b66f780aed50223fef56927482
kanishka kalra <kannukalra1425@gmail.com>
2025-10-17 09:30:08 +0530

    completed the navbar component

---
+++ frontend_Part/src/pages/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Cart =()=>{
+return(
+  <div>
+
+  <h1>hi iam cat</h1>
+  </div>
+)
+
+}
+export default Cart
---
+++ frontend_Part/src/pages/Collection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const  Collection =()=>{
+return(
+  <div>
+
+  hi  i am collection
+  </div>
+)
+
+}
+export default Collection
