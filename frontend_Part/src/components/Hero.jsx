889231319d2e8ea865e9965bde80390ab4cfe831
kanishka kalra <kannukalra1425@gmail.com>
2025-10-19 19:53:24 +0530

    started the hero component

---
+++ frontend_Part/src/components/Hero.jsx
@@ -0,0 +1,14 @@
+
+const Hero =()=>{
+  return(
+    <div className="flex flex-col sm:flex-row border border-gray-400">
+    {/* Hero left side */}
+    <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0">
+    <div>
+      
+    </div>
+    </div>
+    </div>
+  )
+}
+export default Hero
